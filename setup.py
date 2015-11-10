f40d7c7e6a9ec97da931c08c5d5637abbfd74fdb
ctzhu <lei.ctzhu@gmail.com>
2015-11-10 14:23:30 -0500

    Version 0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='vial_detector',
-      version='0.1',
+      version='0.2',
       description='Background adjusted particle detection of fruitfly motion in glass vials',
       long_description=long_description,
       url='http://github.com/ctzhu/vial_detector',
